51b047cf2efa072f4dab7c05fe4706ac66d40b4f
Chiglia <davide.chigliaro33@gmail.com>
2024-02-22 14:26:19 +0000

    provadinuovo

--- public/javascripts/tutti.js
+++ public/javascripts/tutti.js
@@ -12,12 +12,29 @@ function apriMenu(y) {
     y.classList.toggle("change");
 }
 
-function avanti() {
-    const gallery_item_size = document.querySelector('.grid-item').clientWidth;
-    document.getElementById("gallery_scroller").scrollBy(gallery_item_size, 0);
-}
-function indietro() {
-    const gallery_item_size = document.querySelector('.grid-item').clientWidth;
-    document.getElementById("gallery_scroller").scrollBy(-gallery_item_size, 0);
-}
+// function avanti() {
+//     const gallery_item_size = document.querySelector('.grid-item').clientWidth;
+//     document.getElementById("gallery_scroller").scrollBy(gallery_item_size, 3);
+// }
+// function indietro() {
+//     const gallery_item_size = document.querySelector('.grid-item').clientWidth;
+//     document.getElementById("gallery_scroller").scrollBy(-gallery_item_size, 3);
+// }
+            function avanti() {
+                  const gallery_item_size = document.querySelector('.grid-item').clientWidth;
+                  document.getElementById("gallery_scroller").scrollBy({
+                    top: 0,
+                    left: gallery_item_size,
+                    behavior: 'smooth'
+              });
+            }
+
+            function indietro() {
+                  const gallery_item_size = document.querySelector('.grid-item').clientWidth;
+                  document.getElementById("gallery_scroller").scrollBy({
+                        top: 0,
+                        left: -gallery_item_size,
+                        behavior: 'smooth'
+                  });
+            }
 
